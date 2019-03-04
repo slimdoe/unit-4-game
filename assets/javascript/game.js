d5e7d2fe10128b2570e9997feb1c65b52d51c43a
Sy Lim <slimdoe2@gmail.com>
2019-03-03 17:07:30 -0800

    almost

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,11 +2,27 @@ var random_results;
 var lost;
 var win;
 
-for (var i=0; i<4;i++){
-    var crystal = $("<div>");
-
-    console.log ("sy's crystal game");
 
 
-$(".crystal").append(crystal);
+
+
+random_results= Math.floor(Math.random()* 60 )+ 30;
+
+console.log(random_results);
+
+$("#result").html('Sy Crystal Game: ');
+
+for (var i=0;i < 4; i++){
+var random=Math.floor(Math.random()*12);
+console.log (random);
+
+    var crystal = $ ("<div>");
+
+        crystal.attr({
+            "class":'crystal' , "data-random":random
+        });
+
+$(".crystals").append(crystal);
 }
+
+
