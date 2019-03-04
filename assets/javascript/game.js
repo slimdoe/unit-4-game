b930316769545477c060036dd835ccdbe1a30fd2
Sy Lim <slimdoe2@gmail.com>
2019-03-03 20:26:30 -0800

    almost done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,78 @@
 var random_results;
-var lost;
-var win;
+var lost = 0;
+var win = 0;
+var previous = 0;
 
 
 
+var resetandStart = function () {
+    $(".crystals").empty();
+
+    var images = [
+        'https://images.unsplash.com/photo-1521133573892-e44906baee46?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+
+    ]
 
 
-random_results= Math.floor(Math.random()* 60 )+ 30;
+    random_results = Math.floor(Math.random() * 60) + 30;
+};
 
-console.log(random_results);
+//console.log(random_results);
 
-$("#result").html('Sy Crystal Game: ');
+$("#result").html('Sy Crystal Game: ' + random_results);
 
-for (var i=0;i < 4; i++){
-var random=Math.floor(Math.random()*12);
-console.log (random);
+//this generates 4 crystals in <div>
+for (var i = 0; i < 4; i++) {
+    var random = Math.floor(Math.random() * 11) + 1;
+    //console.log (random);
 
-    var crystal = $ ("<div>");
+    var crystal = $("<div>");
 
-        crystal.attr({
-            "class":'crystal' , "data-random":random
-        });
+    crystal.attr({
+        "class": 'crystal', "data-random": random
+    });
 
-$(".crystals").append(crystal);
-}
+    /*crystal.css({
+        "background-image":"url('"+ images[i]+"')",
+        "background-size":"cover"});*/
+    
+    $(".crystals").append(crystal);
 
+    $("#previous").html("total score" + previous);
+    
+};
+
+
+//resetAndStart();
+
+
+//this is an "event delegation"
+$(".crystal").on('click', function () {
+
+    var num = parseInt($(this).attr('data-random'));
+
+    previous += num;
+
+    $("#previous").html("total score" + previous);
+
+    console.log(previous);
+
+    if (previous > random_results) {
+        lost--;
+
+        $("#lost").html("you lost" + lost);
+        previous = 0;
+        resetAndStart();
+    }
+
+    else if (previous === random_results) {
+        win++;
+
+        $("#win").html("you win" + win);
+        previous = 0;
+        resetAndStart();
+
+
+    }
+});
 
